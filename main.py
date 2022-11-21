f2165424505b923aca8b4b88740c254cc835d430
1 <flaimerofficial@yandex.ru>
2022-11-21 18:17:41 +0300

    Сделал построчный вывод

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import sqlite3
 
 
 
+
 def show_table(name_for_show):
     connection = sqlite3.connect('horse_racing.db')
     cursor = connection.cursor()
     sql = "SELECT * FROM " + name_for_show
     cursor.execute(sql)
-    print(cursor.fetchall())
+    for record in cursor:
+        print(record)
+
+    #print(cursor.fetchall())
     connection.close()
 
 print("Добро пожаловать в приложение клуба любителей скачек «RamHorse»!")
@@ -18,26 +22,27 @@ def menu():
     print("Выберите опцию:")
     print("1. Посмотреть информацию о лошадях.")
     print("2. Посмотреть информацию о владельцах лошадей.")
-    print("3. Посмотреть информацию о жокеях")
-    print("4. Посмотреть информацию о состязаниях")
+    print("3. Посмотреть информацию о жокеях.")
+    print("4. Посмотреть информацию о состязаниях.")
     print("-----------------------------------------------")
 
 
 while True:
     menu()
     choice = input("Введите ваш выбор: ")
+    print("-----------------------------------------------")
 
     if choice == "1":
-        print("Лошади")
+        print("Лошади:")
         show_table("horses")
     elif choice == "2":
-        print("Владельцы")
+        print("Владельцы:")
         show_table("owner")
     elif choice == "3":
-        print("Жокеи")
+        print("Жокеи:")
         show_table("rider")
     elif choice == "4":
-        print("Состязания")
+        print("Состязания:")
         show_table("competition")
     else:
-        print("Недействительное значение")
+        print("Недействительное значение.")
