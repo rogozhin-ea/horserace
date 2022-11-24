9f9dff9acfd96c43ba3cb0b3f0249d2bfffd2214
1 <flaimerofficial@yandex.ru>
2022-11-24 22:02:12 +0300

    Функциональные правки

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sqlite3
-import sys
 
 def show_table(name_for_show):
     connection = sqlite3.connect('horse_racing.db')
@@ -27,24 +26,27 @@ def menu():
 
 
 while True:
-    menu()
-    choice = int(input("Введите ваш выбор: "))
-    print("-----------------------------------------------")
+    try:
+        menu()
+        choice = int(input("Введите ваш выбор: "))
+        print("-----------------------------------------------")
 
-    if choice == 1:
-        print("Лошади:")
-        show_table("horses")
-    elif choice == 2:
-        print("Владельцы:")
-        show_table("owner")
-    elif choice == 3:
-        print("Жокеи:")
-        show_table("rider")
-    elif choice == 4:
-        print("Состязания:")
-        show_table("competition")
-    elif choice == 5:
-        break;
-    else:
-        print("Недействительное значение.")
-sys.exit()
+        if choice == 1:
+            print("Лошади:")
+            show_table("horses")
+        elif choice == 2:
+            print("Владельцы:")
+            show_table("owner")
+        elif choice == 3:
+            print("Жокеи:")
+            show_table("rider")
+        elif choice == 4:
+            print("Состязания:")
+            show_table("competition")
+        elif choice == 5:
+            break;
+        else:
+            print("Недействительное значение.")
+
+    except EOFError:
+        print("Exception handled")
