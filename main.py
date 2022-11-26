07be4232fb06906106c3e51208d459fbcefe7bc0
1 <flaimerofficial@yandex.ru>
2022-11-26 14:32:24 +0300

    Изменение вывода лошадей

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def show_table(name_for_show):
 
     connection.close()
 
+
 def show_norm_table(option):
     connection = sqlite3.connect('horse_racing.db')
     cursor = connection.cursor()
@@ -45,6 +46,102 @@ def show_norm_table(option):
 
             print(name_string, gen_string, age_string)
 
+    elif option == 2:
+        sql = "SELECT * FROM owner"
+        cursor.execute(sql)
+        records = cursor.fetchall()
+        num = (len(records))
+        for i in range(1, num):
+            name = "SELECT Name FROM owner where id = " + str(i)
+            cursor.execute(name)
+            name_record = cursor.fetchone()
+            name_string = str(name_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
+            if len(name_string) < 35:
+                diff = 35 - len(name_string)
+                for k in range(1, diff):
+                    name_string = name_string + " "
+
+            address = "SELECT Address FROM owner where id = " + str(i)
+            cursor.execute(address)
+            address_record = cursor.fetchone()
+            address_string = str(address_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
+            if len(address_string) < 62:
+                diff = 62 - len(address_string)
+                for k in range(1, diff):
+                    address_string = address_string + " "
+
+            age = "SELECT Age FROM owner where id = " + str(i)
+            cursor.execute(age)
+            age_record = cursor.fetchone()
+            age_string = str(age_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
+
+            print(name_string, address_string, age_string)
+
+    elif option == 3:
+        sql = "SELECT * FROM rider"
+        cursor.execute(sql)
+        records = cursor.fetchall()
+        num = (len(records))
+        for i in range(1, num):
+            name = "SELECT Name FROM rider where id = " + str(i)
+            cursor.execute(name)
+            name_record = cursor.fetchone()
+            name_string = str(name_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
+            if len(name_string) < 30:
+                diff = 30 - len(name_string)
+                for k in range(1, diff):
+                    name_string = name_string + " "
+
+            address = "SELECT Address FROM rider where id = " + str(i)
+            cursor.execute(address)
+            address_record = cursor.fetchone()
+            address_string = str(address_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
+            if len(address_string) < 62:
+                diff = 62 - len(address_string)
+                for k in range(1, diff):
+                    address_string = address_string + " "
+
+            age = "SELECT Age FROM rider where id = " + str(i)
+            cursor.execute(age)
+            age_record = cursor.fetchone()
+            age_string = str(age_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "") + "      "
+
+            rating = "SELECT rating FROM rider where id = " + str(i)
+            cursor.execute(rating)
+            rating_record = cursor.fetchone()
+            rating_string = str(rating_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
+
+            print(name_string, address_string, age_string, rating_string)
+
+    elif option == 4:
+        sql = "SELECT * FROM horses"
+        cursor.execute(sql)
+        records = cursor.fetchall()
+        num = (len(records))
+        for i in range(1, num):
+            name = "SELECT Nickname FROM horses where id = " + str(i)
+            cursor.execute(name)
+            name_record = cursor.fetchone()
+            name_string = str(name_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
+            if len(name_string) < 12:
+                diff = 12 - len(name_string)
+                for k in range(1, diff):
+                    name_string = name_string + " "
+
+            gender = "SELECT Gender FROM horses where id = " + str(i)
+            cursor.execute(gender)
+            gen_record = cursor.fetchone()
+            gen_string = str(gen_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "") + "     "
+
+            age = "SELECT Age FROM horses where id = " + str(i)
+            cursor.execute(age)
+            age_record = cursor.fetchone()
+            age_string = str(age_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
+
+            print(name_string, gen_string, age_string)
+
+    connection.close()
+
 
 print("Добро пожаловать в приложение клуба любителей скачек «RamHorse»!")
 
@@ -61,24 +158,26 @@ def menu():
 
 
 while True:
-        menu()
-        choice = int(input("Введите ваш выбор: "))
-        print("-----------------------------------------------")
+    menu()
+    choice = int(input("Введите ваш выбор: "))
+    print("-----------------------------------------------")
 
-        if choice == 1:
-            print("Лошади:")
-            print("Имя         Пол         Возраст")
-            show_norm_table(1)
-        elif choice == 2:
-            print("Владельцы:")
-            show_table("owner")
-        elif choice == 3:
-            print("Жокеи:")
-            show_table("rider")
-        elif choice == 4:
-            print("Состязания:")
-            show_table("competition")
-        elif choice == 5:
-            break;
-        else:
-            print("Недействительное значение.")
+    if choice == 1:
+        print("Лошади:")
+        print("Имя         Пол         Возраст")
+        show_norm_table(1)
+    elif choice == 2:
+        print("Владельцы:")
+        print("Имя                                Адрес                                                         Возраст")
+        show_norm_table(2)
+    elif choice == 3:
+        print("Жокеи:")
+        print("Имя                           Адрес                                                         Возраст  Рейтинг")
+        show_norm_table(3)
+    elif choice == 4:
+        print("Состязания:")
+        show_table("competition")
+    elif choice == 5:
+        break;
+    else:
+        print("Недействительное значение.")
