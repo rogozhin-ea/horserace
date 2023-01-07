658d0aff83f7c49b75e01f3d190bedc9fb273d79
1 <flaimerofficial@yandex.ru>
2023-01-07 20:10:29 +0300

    Добавил ввод в базу данных

--- main.py
+++ main.py
@@ -27,21 +27,6 @@ def auth(login, password):
         return fl_auth
 
 
-def show_table(name_for_show):
-    connection = sqlite3.connect('horse_racing.db')
-    cursor = connection.cursor()
-    if name_for_show == "horses":
-        sql = "SELECT * "
-    sql = "SELECT DISTINCT competition.name, competition.date_and_time, competition.address, competition.hippodrome_name, competition_result.places, rider.Name, horses.Nickname, competition_result.driving_time from competition_result JOIN competition ON competition_result.id_competiton = competition.id JOIN competition_riders_horses ON competition_result.id_competiton = competition_riders_horses.id_competition JOIN horses_and_riders ON competition_result.id_riders = horses_and_riders.id_riders JOIN rider ON horses_and_riders.id_riders = rider.ID JOIN horses ON horses_and_riders.id_horses = horses.id ORDER BY competition.name"
-    cursor.execute(sql)
-    records = cursor.fetchall()
-    for row in records:
-        row_string = str(row).replace("(", "").replace(")", "").replace("'", "")
-        print(row_string)
-    connection.close()
-    return 'Success'
-
-
 def show_norm_table(option):
     connection = sqlite3.connect('horse_racing.db')
     cursor = connection.cursor()
@@ -71,7 +56,6 @@ def show_norm_table(option):
             age_string = str(age_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
 
             print(name_string, gen_string, age_string)
-            return name_string.replace(" ", ""), gen_string.replace(" ", ""), age_string.replace(" ", "")
 
     elif option == 2:
         sql = "SELECT * FROM owner"
@@ -103,7 +87,6 @@ def show_norm_table(option):
             age_string = str(age_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
 
             print(name_string, address_string, age_string)
-            return name_string, address_string, age_string
 
     elif option == 3:
         sql = "SELECT * FROM rider"
@@ -140,35 +123,18 @@ def show_norm_table(option):
             rating_string = str(rating_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
 
             print(name_string, address_string, age_string, rating_string)
-            return name_string, address_string, age_string, rating_string
 
     elif option == 4:
-        sql = "SELECT * FROM horses"
+        connection = sqlite3.connect('horse_racing.db')
+        cursor = connection.cursor()
+        sql = "SELECT DISTINCT competition.name, competition.date_and_time, competition.address, competition.hippodrome_name, competition_result.places, rider.Name, horses.Nickname, competition_result.driving_time from competition_result JOIN competition ON competition_result.id_competiton = competition.id JOIN competition_riders_horses ON competition_result.id_competiton = competition_riders_horses.id_competition JOIN horses_and_riders ON competition_result.id_riders = horses_and_riders.id_riders JOIN rider ON horses_and_riders.id_riders = rider.ID JOIN horses ON horses_and_riders.id_horses = horses.id ORDER BY competition.name"
         cursor.execute(sql)
         records = cursor.fetchall()
-        num = (len(records))
-        for i in range(1, num):
-            name = "SELECT Nickname FROM horses where id = " + str(i)
-            cursor.execute(name)
-            name_record = cursor.fetchone()
-            name_string = str(name_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
-            if len(name_string) < 12:
-                diff = 12 - len(name_string)
-                for k in range(1, diff):
-                    name_string = name_string + " "
-
-            gender = "SELECT Gender FROM horses where id = " + str(i)
-            cursor.execute(gender)
-            gen_record = cursor.fetchone()
-            gen_string = str(gen_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "") + "     "
-
-            age = "SELECT Age FROM horses where id = " + str(i)
-            cursor.execute(age)
-            age_record = cursor.fetchone()
-            age_string = str(age_record).replace("(", "").replace(")", "").replace("'", "").replace(",", "")
-
-            print(name_string, gen_string, age_string)
-            return name_string, gen_string, age_string
+        for row in records:
+            row_string = str(row).replace("(", "").replace(")", "").replace("'", "")
+            print(row_string)
+        connection.close()
+        return 'Success'
 
     else:
         msg = "Недействительное значение."
@@ -178,75 +144,69 @@ def show_norm_table(option):
     connection.close()
 
 
-def enter_table(login, password):
-    print("Временно недоступно")
-
-'''
-def enter_riders():
-    while True:
-        host = "192.168.31.90"
-        user = "postgres"
-        password = "secret"
-        db_name = "horse_riders"
-        port = "5432"
-
-        try:
-            connection = psycopg2.connect(user=user, password=password, host=host, port=port)
-            connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        except:
-            print("Ошибка подключения")
-            break;
-
-        cursor = connection .cursor()
-        cursor.execute(f"CREATE DATABASE {db_name};")
-        print("База данных " + db_name + " успешно созданна" )
-
+def enter_table(db_name):
+    if db_name == 'horse':
+        h_name = input("Введите кличку: ")
+        h_gen = input("Введите пол: ")
+        h_age = input("Введите возраст: ")
+        h_owner = input("Введите id владельца: ")
+        connection = sqlite3.connect('horse_racing.db')
+        cursor = connection.cursor()
+        sql = f"INSERT INTO horses (Nickname, Gender, Age, id_owner) VALUES ('{h_name}', '{h_gen}', '{h_age}', '{h_owner}');"
+        cursor.execute(sql)
+        connection.commit()
+        cursor.execute("SELECT * FROM horses")
+        print(cursor.fetchall())
         connection.close()
+        return 'Success'
 
-        try:
-            connection = psycopg2.connect(user=user, password=password, host=host, port=port, dbname=db_name)
-            connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        except:
-            print("Ошибка подключения")
-            break;
-        try:
-            cursor = connection.cursor()
-            cursor.execute("""CREATE TABLE riders (id SERIAL NOT NULL PRIMARY KEY, name CHARACTER VARYING(30),email CHARACTER VARYING(30),age INT);""")
-        except:
-            print("Ошибка создания таблицы")
-            break;
-
-        count_riders = input("Введите количество новых жокеев: ")
-
-        for i in range(int(count_riders)):
-            rider = input("Введите имя, email и возраст жокея: ").replace('"' , "'")
-            cursor.execute(f"""INSERT INTO public.riders (name,email,age) VALUES ({rider});""")
-            connection.commit()
-            cursor.execute("""SELECT * FROM public.riders """)
-            print(cursor.fetchall())
-
+    elif db_name == 'owner':
+        o_name = input("Введите ФИО: ")
+        o_address = input("Введите адрес: ")
+        o_age = input("Введите возраст: ")
+        connection = sqlite3.connect('horse_racing.db')
+        cursor = connection.cursor()
+        sql = f"INSERT INTO owner (name, address, age) VALUES ('{o_name}', '{o_address}', '{o_age}');"
+        cursor.execute(sql)
+        connection.commit()
+        cursor.execute("SELECT * FROM owner")
+        print(cursor.fetchall())
         connection.close()
-        break;
+        return 'Success'
 
-def show_riders ():
-    host = "192.168.31.90"
-    user = "postgres"
-    password = "secret"
-    db_name = "horse_riders"
-    port = "5432"
+    elif db_name == 'rider':
+        r_name = input("Введите ФИО: ")
+        r_address = input("Введите адрес: ")
+        r_age = input("Введите возраст: ")
+        r_rating = input("Введите рейтинг: ")
+        connection = sqlite3.connect('horse_racing.db')
+        cursor = connection.cursor()
+        sql = f"INSERT INTO rider (Name, Address, Age, rating) VALUES ('{r_name}', '{r_address}', '{r_age}', '{r_rating}');"
+        cursor.execute(sql)
+        connection.commit()
+        cursor.execute("SELECT * FROM rider")
+        print(cursor.fetchall())
+        connection.close()
+        return 'Success'
 
-    try:
-        connection = psycopg2.connect(user=user, password=password, host=host, port=port, dbname=db_name)
-        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    except:
-        print("Ошибка подключения")
+    elif db_name == 'competition':
+        c_name = input("Введите название соревнований: ")
+        c_date = input("Введите дату: ")
+        c_hyp = input("Введите название ипподрома: ")
+        c_address = input("Введите адрес: ")
+        connection = sqlite3.connect('horse_racing.db')
+        cursor = connection.cursor()
+        sql = f"INSERT INTO competition (name, date_and_time, hippodrome_name, address) VALUES ('{c_name}', '{c_date}', '{c_hyp}', '{c_address}');"
+        cursor.execute(sql)
+        connection.commit()
+        cursor.execute("SELECT * FROM competition")
+        print(cursor.fetchall())
+        connection.close()
+        return 'Success'
 
-    cursor = connection.cursor()
-    cursor.execute("""SELECT * FROM public.riders """)
-    print(cursor.fetchall())
+    else:
+        print("Недействительное значение")
 
-    connection.close()
-'''
 
 print("Добро пожаловать в приложение клуба любителей скачек «RamHorse»!")
 
@@ -258,9 +218,9 @@ def menu():
     print("2. Посмотреть информацию о владельцах лошадей.")
     print("3. Посмотреть информацию о жокеях.")
     print("4. Посмотреть информацию о состязаниях.")
-    print("5. Добавить информацию о жокеях")
-    print("6. Добавить информацию о лошадях")
-    print("7. Добавить информацию о владельцах лошадей")
+    print("5. Добавить информацию о лошадях")
+    print("6. Добавить информацию о владельцах лошадей")
+    print("7. Добавить информацию о жокеях")
     print("8. Добавить информацию о состязаниях")
     print("9. Выход.")
     print("-----------------------------------------------")
@@ -286,30 +246,29 @@ def inf_menu(fl_auth):
             print("Имя                                Адрес                                                         Возраст")
             show_norm_table(2)
         elif choice == "3":
-            print("Жокеи:")
-            #show_riders()
+            print("Имя                           Адрес                                                         Возраст  Рейтинг")
+            show_norm_table(3)
         elif choice == "4":
             print("Состязания:")
-            show_table("competition")
+            show_norm_table(4)
         elif choice == "5":
             if fl_auth == 1:
-                print("Yes")
-                #enter_riders()
+                enter_table('horse')
             else:
                 print("Для продолжения необходимо авторизоваться")
         elif choice == "6":
             if fl_auth == 1:
-                enter_table()
+                enter_table('owner')
             else:
                 print("Для продолжения необходимо авторизоваться")
         elif choice == "7":
             if fl_auth == 1:
-                enter_table()
+                enter_table('rider')
             else:
                 print("Для продолжения необходимо авторизоваться")
         elif choice == "8":
             if fl_auth == 1:
-                enter_table()
+                enter_table('competition')
             else:
                 print("Для продолжения необходимо авторизоваться")
         elif choice == "9":
@@ -338,4 +297,4 @@ def start_menu():
             print("Недействительное значение.")
 
 
-#start_menu()
+start_menu()
