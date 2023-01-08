3ba836d532ecdce12fd20c3d311e3bf816c2575e
KirillKor21 <77528678+KirillKor21@users.noreply.github.com>
2023-01-09 01:55:27 +0300

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,7 +1,7 @@
 import os
 import json
 pycodestyle_check = os.system('python3 -m pycodestyle main.py > /dev/null 2>&1')
-os.sustem('python3 -m bandit -r main.py -f json -o result.json > /dev/null 2>&1')
+os.system('python3 -m bandit -r main.py -f json -o result.json > /dev/null 2>&1')
 with open("result.json", "r") as word:
     bandit = json.load(word)
 if bandit["results"] or pycodestyle_check:
